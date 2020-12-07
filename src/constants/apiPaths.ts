43a8b5edca7f775ce5dbc0cd33555140b4853d50
Anisotropic <skolzyashiy@gmail.com>
2020-12-07 22:11:12 +0300

    Update "cart" api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://5zikxo0ct8.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://uv2t74mcu0.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://anisotropic-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
