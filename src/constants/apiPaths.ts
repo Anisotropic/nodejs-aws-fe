0d2d2d55e0fd4938f151b5a2b0ecdffe2f039bbc
Anisotropic <skolzyashiy@gmail.com>
2020-12-14 20:52:45 +0300

    Change back "import" path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'http://anisotropic-bff-api.eu-west-1.elasticbeanstalk.com/products/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'http://anisotropic-bff-api.eu-west-1.elasticbeanstalk.com/import',
+  import: 'https://5zikxo0ct8.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'http://anisotropic-bff-api.eu-west-1.elasticbeanstalk.com/products',
   cart: 'http://anisotropic-bff-api.eu-west-1.elasticbeanstalk.com/cart/api',
 };
